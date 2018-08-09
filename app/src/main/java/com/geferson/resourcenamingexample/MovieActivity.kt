e2fae95e66ce7da4c0851bdb4069bfef55e33683
Geferson Pillaca <gpillaca@fandango.com>
2018-08-09 11:33:49 -0500

    add xml naming convention

---
+++ app/src/main/java/com/geferson/resourcenamingexample/MovieActivity.kt
@@ -0,0 +1,11 @@
+package com.geferson.resourcenamingexample
+
+import android.app.Activity
+import android.os.Bundle
+
+class MovieActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+    }
+}
